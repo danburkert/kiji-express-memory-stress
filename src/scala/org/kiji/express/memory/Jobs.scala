71b04701676a8e7b76ee92d74689b1e449203476
Dan Burkert <danburkert@gmail.com>
2013-11-20 19:06:18 -0800

    backwards port changes for 0.9.0

--- src/scala/org/kiji/express/memory/Jobs.scala
+++ src/scala/org/kiji/express/memory/Jobs.scala
@@ -1,8 +1,8 @@
 package org.kiji.express.memory
 
 import com.twitter.scalding.{Tsv, Args}
-import org.kiji.express.flow._
-import scala.util.Random
+import org.kiji.express.{PagedKijiSlice, KijiSlice}
+import org.kiji.express.flow.{All, KijiOutput, QualifiedColumn, KijiInput, KijiJob}
 
 /**
  * Counts the number of versions in each row of a column.
@@ -15,12 +15,12 @@ class CountColumnVersions(args: Args) extends KijiJob(args) {
   val uri = args.getOrElse("uri", "kiji://localhost:2181/default/memory_stress")
   val out = args.getOrElse("output", "output/count-column-versions")
 
-  val column = QualifiedColumnInputSpec(family, qualifier, all, paging = PagingSpec.Cells(1000))
+  val column = QualifiedColumn(family, qualifier).withPaging(1000).useDefaultReaderSchema()
   val output = Tsv(out)
 
-  KijiInput(uri, Map(column -> 'a))
+  KijiInput(uri)(Map(column -> 'a))
     .read
-    .mapTo('a -> 'count) { a: Seq[FlowCell[_]] => a.length }
+    .mapTo('a -> 'count) { a: PagedKijiSlice[_] => a.size }
     .write(output)
 }
 
@@ -35,12 +35,12 @@ class CountColumnVersionsTwice(args: Args) extends KijiJob(args) {
   val uri = args.getOrElse("uri", "kiji://localhost:2181/default/memory_stress")
   val out = args.getOrElse("output", "output/count-column-versions-twice")
 
-  val column = QualifiedColumnInputSpec(family, qualifier, all, paging = PagingSpec.Cells(1000))
+  val column = QualifiedColumn(family, qualifier).withPaging(1000).useDefaultReaderSchema()
   val output = Tsv(out)
 
-  KijiInput(uri, Map(column -> 'a))
+  KijiInput(uri)(Map(column -> 'a))
       .read
-      .mapTo('a -> 'count) { a: Seq[FlowCell[_]] => a.length }
+      .mapTo('a -> 'count) { a: PagedKijiSlice[_] => a.size }
       .write(output)
 }
 
@@ -55,12 +55,13 @@ class CountColumnCells(args: Args) extends KijiJob(args) {
   val uri = args.getOrElse("uri", "kiji://localhost:2181/default/memory_stress")
   val out = args.getOrElse("output", "output/count-column-cells")
 
-  val column = QualifiedColumnInputSpec(family, qualifier, all, paging = PagingSpec.Cells(1000))
+  val column = QualifiedColumn(family, qualifier).withPaging(1000).useDefaultReaderSchema()
   val output = Tsv(out)
 
-  KijiInput(uri, Map(column -> 'a))
+  KijiInput(uri)(Map(column -> 'a))
     .read
-    .flatten[Seq[FlowCell[_]]]('a -> 'b)
+    .mapTo('a -> 'b) { a: KijiSlice[_] => a.cells }
+    .flattenTo[Seq[_]]('b -> 'c)
     .groupAll { _.size }
     .write(output)
 }
@@ -71,12 +72,12 @@ class WriteRandomLong(args: Args) extends KijiJob(args) {
   val outQualifier = args.getOrElse("outQualifier", "long")
   val uri = args.getOrElse("uri", "kiji://localhost:2181/default/memory_stress")
 
-  val inColumn = QualifiedColumnInputSpec(family, inQualifier, all)
-  val outColumn = QualifiedColumnOutputSpec(family, outQualifier)
+  val inColumn = QualifiedColumn(family, inQualifier).useDefaultReaderSchema()
+  val outColumn = QualifiedColumn(family, outQualifier).useDefaultReaderSchema()
 
-  KijiInput(uri, Map(inColumn -> 'a))
+  KijiInput(uri)(Map(inColumn -> 'a))
     .read
-    .map('a -> 'b) { a: Seq[_] => System.currentTimeMillis() }
-    .write(KijiOutput(uri, Map('b -> outColumn)))
+    .map('a -> 'b) { a: KijiSlice[_] => System.currentTimeMillis() }
+    .write(KijiOutput(uri)(Map('b -> outColumn)))
 
 }
